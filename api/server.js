3b3e1cfc159be13407426a34e8f58a82c1ae6210
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-09-06 14:58:36 -0400

    enabled cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const productsRouter = require('../products/products-router');
 const actionsRouter = require('../actions/actions-router');
 const server = express()
 
 //global middleware
 server.use(express.json())
+server.use(helmet());
+server.use(cors())
+server.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin")
+    next()
+})
 
 server.use('/products', productsRouter);
 server.use('/actions', actionsRouter);
