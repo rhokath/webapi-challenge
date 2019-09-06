56ea31abd437e858a20181972c82c5a4d3752a21
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-09-06 13:56:20 -0400

    almost there

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -34,28 +34,102 @@ router.get('/:id', (req, res) => {
     })
     
 });
-router.post('/', (req, res) => {
-    
-    
-});
-router.delete('/', (req, res) => {
-    // Actions.remove(req.params.id)
-    // .then(count =>{
-    //     if(count && count > 0){
-    //         res.status(200).json({message: "the action was successfully deleted"})
-    //     } else {
-    //         res.status(404).json({errorMessage: "the action with the specified ID does not exists"})
+router.post('/:id', validatePostingAction, (req, res) => {
+    const post = req.body
 
-    //     }
-    // })
-    // .catch(err =>{
-    //     console.log("error in delete action", err)
-    //     res.status(500).json({errorMessage: "there was a problem deleting the action"})
-    // })
+    Actions.insert(post)
+        .then(post => {
+           res.status(201).json(post);
+        })
+        .catch(()=> {
+            res.status(500).json({
+                errorMessage: "can't make that post happen"
+            })
+        })
     
 });
-router.put('/', (req, res) => {
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    Actions.remove(id)
+    .then(action => {
+        if(action){
+            res.status(200).json({
+                message: "the action was deleted."
+            })
+        } else {
+            res.status(404).json({
+                message: "the action with the specified ID doesn't exist."
+            })
+        }
+    })
+    .catch((err)=> {
+        console.log("error in delete of action***", err)
+        res.status(500).json({
+            errorMessage: "The action could not be removed."
+        })
+    })
+    
     
 });
+router.put('/:id', (req, res) => {
+    const { description, notes} = req.body;
+    if(!description || ! notes){
+        res.status(400).json({
+            errorMessage: 'please provide name and bio for the user.'
+        });
+    } else {
+        Actions.update(req.params.id, req.body)
+        .then(action => {
+            if (action){
+                res.status(200).json(action);
+            } else {
+                res.status(404).json({
+                    message: 'the action with the specified ID does not exist.'
+                });
+            }
+        })
+        .catch(()=> {
+            res.status(500).json({
+                errorMessage: 'The action information could not be modified.'
+            })
+        })
+    }
+    
+});
+
+//validate action
+function validatePostingAction(req, res, next){
+    const { id : project_id} = req.params;
+    const { description, notes} = req.body;
+    if(!req.body){
+        return res.status(400).json({message: "missing data"})
+    }
+    if(!description || !notes){
+        return res.status(400).json({message: "please fill in all fields"})
+    }
+    if(description.length > 128){
+        return res.status(400).json({message: "description can only be 128 characters long"})
+    }
+    req.body = {project_id, description, notes}
+    next()
+
+}
+//validate action
+function validateAction(req, res, next){
+    const {id} = req.params;
+    Products.getProjectActions(id)
+    .then(actions =>{
+        if(actions){
+            res.status(200).json(actions)
+            next()
+        } else {
+            res.status(400).json({message: "the actions for that project don't exist"})
+        }
+    })
+    .catch((err)=>{
+        console.log("error in validateAction", err)
+        res.status(500).json({errorMessage: "there was a problem validating actions"})
+    })
+}
 
 module.exports = router;
