8b337e411220f15ed18979d5765685c5fc3d2c1b
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-09-06 12:59:09 -0400

    completed routes for products

--- products/products-router.js
+++ products/products-router.js
@@ -36,12 +36,60 @@ router.get('/:id', (req, res) => {
     
 });
 router.post('/', (req, res) => {
+    const {name, description} = req.body;
+    if(!name || ! description){
+       res.status(400).json({errorMessage: "please provide name and description for the product"}) 
+    } else{
+        Products.insert(req.body)
+            .then(product =>{
+                res.send(201).json(product)
+            })   
+            .catch(err =>{
+                console.log("error in post of product", err)
+                res.status(500).json({errorMessage: "there was a problem adding the product to the database"})
+            })
+    }
     
 });
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
+    Products.remove(req.params.id)
+    .then(count =>{
+        if(count && count > 0){
+            res.status(200).json({message: "the product was successfully deleted"})
+        } else {
+            res.status(404).json({errorMessage: "the product with the specified ID does not exists"})
+
+        }
+    })
+    .catch(err =>{
+        console.log("error in delete product", err)
+        res.status(500).json({errorMessage: "there was a problem deleting the project"})
+    })
     
 });
-router.put('/', (req, res) => {
+router.put('/:id', (req, res) => {
+    const { name, description} = req.body;
+    if(!name || ! description){
+        res.status(400).json({
+            errorMessage: 'please provide name and description for the product.'
+        });
+    } else {
+        Products.update(req.params.id, req.body)
+        .then(product => {
+            if (product){
+                res.status(200).json(req.body);
+            } else {
+                res.status(404).json({
+                    message: 'the product with the specified ID does not exist.'
+                });
+            }
+        })
+        .catch(()=> {
+            res.status(500).json({
+                errorMessage: 'The product information could not be modified.'
+            })
+        })
+    }
     
 });
 
