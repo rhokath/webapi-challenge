e4e0fb658c5b8f287da4e32f77542c570b6f2cce
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-09-06 12:23:55 -0400

    completed get requests for products and actions

--- products/products-router.js
+++ products/products-router.js
@@ -1,9 +1,49 @@
-const express = require('express')
+const express = require('express');
+const Actions = require('../data/helpers/actionModel');
+const Products = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.send('get to /products/')
+    Products.get()
+    .then(products => {
+        res.status(200).json(products)
+    })
+    .catch(err =>{
+        console.log("error in get products", err)
+        res.status(500).json({
+            errorMessage: "The products info could not be retrieved"
+        })
+    })
 })
+router.get('/:id', (req, res) => {
+    const {id}= req.params;
+    Products.get(id)
+    .then(product => {
+        if(product){
+        res.status(200).json(product)
+        } else {
+            res.status(400).json({message: "the product with that id does not exists"})
+        }
+    })
+    .catch(err =>{
+        console.log("error in get actions", err)
+        res.status(500).json({
+            errorMessage: "The actions info could not be retrieved"
+        })
+    })
+    
+    
+});
+router.post('/', (req, res) => {
+    
+});
+router.delete('/', (req, res) => {
+    
+});
+router.put('/', (req, res) => {
+    
+});
+
 
 module.exports = router;
